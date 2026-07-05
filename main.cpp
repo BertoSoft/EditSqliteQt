05314a061dc85dcf55f63a2d2c5828266d9a1e81
alberto-forjan-tu-as <berto.forjan@gmail.com>
2026-07-05 12:13:16 +0200

    050720261212-1-portatil

--- main.cpp
+++ main.cpp
@@ -7,14 +7,5 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
-
-
-    //
-    // Comprobar commit portatil
-    //
-
-
-
     return QApplication::exec();
 }
